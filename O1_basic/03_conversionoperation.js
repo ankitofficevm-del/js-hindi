f3b0e848ee516cd843a576483cf177002c823df1
ankitofficevm-del <ankitofficevm@gmail.com>
2025-08-26 08:14:37 +0000

    conversion of datatypes

--- O1_basic/03_conversionoperation.js
+++ O1_basic/03_conversionoperation.js
@@ -1,4 +1,41 @@
-let score = "44"
+let score = "44abc"  //agr hmm issko double quote mein rhkte hai yhe output string mein dega
+let mark = 55    // agr hmm issko bina quote ke rhkte hai yhe output number mein dega
+let scores = null  // agr hmm null se krte hai ky value hai aate hai
+let scoress = undefined
+let scoresss = true // age hmm booelan value krte hai dekhte hai ky value aayegi
+let scoressss = "Ankit" // agr string hoti hai ky value aayegi
 
-console.log(typeof score);
-console.log(typeof (score));
+
+//console.log(typeof score); 
+//console.log(typeof (mark));
+
+//yhe convert kra hai number ko
+let valueInNumber = Number(scoressss)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // yhe value check krne ke liye kra hai
+
+
+// "44" => 44
+// "44abc" => NaN  not a number
+// true => 1; false => 0
+
+
+
+// convert krte hai boolen mein 
+let isLoggedIn = "Ankit"
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoogedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ankit" => true
+
+
+
+// convert string mein krte hai 
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
