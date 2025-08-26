d9f3472d5afa19f1fb2fffb1db0ec84f04a06275
ankitofficevm-del <ankitofficevm@gmail.com>
2025-08-26 07:26:58 +0000

    datatypes

--- O1_basic/02_datatypes.js
+++ O1_basic/02_datatypes.js
@@ -1,3 +1,35 @@
 "use strict";  //treat all js code as newer version  
 
 // alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3)  // code readability should be high
+
+console.log("Ankit")
+
+
+let name = "Ankit" //yhe hogya string datatypes
+let age = 24 // yhe hogya numbers
+let isLoggedIn = true // or false yhe hogya boolean types
+//let state; // issliye aata hai undefined
+//let state = null  //isske bolte hai empty 
+
+
+// primitive data type 
+// number => 2 to power 53 yhe range hai number ke
+// bigint //agr number jda bada ho usske liye use kiya jta hai jese stock or trading
+// string => "" // yhe datatype issko single quote yha double quote mein use kr skte hai pr double qoute prefer krna chaiye
+// boolean => true/false // yha server se detail aaya yha nhi
+// null //stand alone value hai js ke under
+// undefined =>  // isse bolte jese koi value define nhi kre ho
+// symbol  //uniqueness ke liye use hota hai
+
+
+// object
+
+
+console.log(typeof age) // isske khte hai kisska typeof pta krna
+console.log(typeof undefined) // isske type undefined hai
+console.log(typeof null)  // isska type object hai
+console.log(typeof "Ankit")
